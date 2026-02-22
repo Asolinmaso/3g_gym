1d69f373de0abdb6e98eab78c56ad91c4451b92d
Asolinmaso <150114681+Asolinmaso@users.noreply.github.com>
2026-02-22 18:37:04 +0530

    Update Pricing.tsx

--- components/pricing/Pricing.tsx
+++ components/pricing/Pricing.tsx
@@ -76,11 +76,6 @@ export default function Pricing() {
     <section id="pricing" className="pricing-section">
       <div className="content-inner">
         <div className="pricing-title-wrap">
-          <span className="pricing-title-icon" aria-hidden>
-            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M20.57 14.86L22 13.43L20.57 12L17 15.57L8.43 7L12 3.43L10.57 2L9.14 3.43L7.71 2L5.57 4.14L4.14 2.71L2.71 4.14L4.14 5.57L2 7.71L3.43 9.14L2 10.57L3.43 12L7 8.43L15.57 17L12 20.57L13.43 22L14.86 20.57L16.29 22L18.43 19.86L19.86 21.29L21.29 19.86L19.86 18.43L22 16.29L20.57 14.86Z" fill="white" fillOpacity="0.9" />
-            </svg>
-          </span>
           <h2 className="pricing-title">
             <span className="pricing-title__gradient">Choose the </span>
             <span className="pricing-title__accent">Plan</span>
