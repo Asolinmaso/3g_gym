d972551454bcd2d4f208053a8a1baed8cfc0a993
Asolinmaso <150114681+Asolinmaso@users.noreply.github.com>
2026-02-28 11:16:46 +0530

    Add files via upload

--- app/career/page.tsx
+++ app/career/page.tsx
@@ -1,11 +1,24 @@
+'use client';
 import { CareerApply, CareerHero, OpenPositions } from "@/features/career";
+import Image from "next/image";
+import { useSlideUpOnScroll } from "@/hooks/useSlideUpOnScroll";
 
 export default function CareerPage() {
+   const equipmentRef = useSlideUpOnScroll<HTMLElement>();
   return (
     <main className="career-page flex-1">
       <CareerHero />
       <OpenPositions />
       <CareerApply />
+      <section className="footer-equipment" ref={equipmentRef}>
+        <Image
+          src="/footer_image2.png"
+          alt="Gym Equipment"
+          width={300}
+          height={400}
+          className="footer-equipment-img"
+        />
+      </section>
     </main>
   );
 }
